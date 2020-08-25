3fc53c74ca74ee6d0d0a0dadc176ec40d37840f6
dgotlieb <Dgotlieb8@gmail.com>
2020-08-25 19:56:54 +0300

    Update rest_test.py

--- requests_test/rest_test.py
+++ requests_test/rest_test.py
@@ -2,18 +2,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# local users storage
+users = {}
 # supported methods
 @app.route('/data/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def user(user_id):
     if request.method == 'GET':
-        return {'user_id': user_id}, 200 # status code
+        return {'user_id': user_id, 'user_name': users[user_id]}, 200 # status code
 
     elif request.method == 'POST':
         # getting the json data payload from request
         request_data = request.json
         # treating request_data as a dictionary to get a specific value from key
         user_name = request_data.get('user_name')
-        return {'user id': user_id , 'user name': user_name}, 200 # status code
+        users[user_id] = user_name
+        return {'user id': user_id , 'user name': user_name, 'status': 'saved'}, 200 # status code
   # todo elif for put and delete
 
 
