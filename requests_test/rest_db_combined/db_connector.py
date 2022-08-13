ce0b4cc7ddffc62e18406c632889c238cfbbdac5
dgotlieb <Dgotlieb8@gmail.com>
2022-08-13 15:59:41 +0300

    Create db_connector.py

---
+++ requests_test/rest_db_combined/db_connector.py
@@ -0,0 +1,17 @@
+import pymysql
+
+
+def add_user(user_id, username):
+    schema_name = 'aaa'
+    # Establishing a connection to DB
+    conn = pymysql.connect(host='remotemysql.com', port=3306, user='root', passwd='123', db=schema_name)
+    conn.autocommit(True)
+
+    # Getting a cursor from Database
+    cursor = conn.cursor()
+
+    # Inserting data into table
+    cursor.execute(f"INSERT into {schema_name}.users (name, id) VALUES ('{username}', {user_id})")
+
+    cursor.close()
+    conn.close()
